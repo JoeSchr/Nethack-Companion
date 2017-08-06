e68723dcde5acf9485ab4abed66d053dd8c23756
Joe <joe.vandelay@gmail.com>
2017-08-06 22:09:53 +0200

    adds bable-flow to npm

--- index.android.js
+++ index.android.js
@@ -1,29 +1,28 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * @flow
  */
 
-import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
 
 export default class NHcalc extends Component {
@@ -33,12 +32,12 @@ export default class NHcalc extends Component {
         <Text style={styles.welcome}>Hello World </Text>
         <Text style={styles.instructions}>Wie gehts Euch´? everything</Text>
         <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+          Double tap R on your keyboard to reload,{"\n"}
+          And then hope for the best! Shake or press menu button for dev menu
         </Text>
       </View>
     );
   }
 }
 
-AppRegistry.registerComponent('NHcalc', () => NHcalc);
+AppRegistry.registerComponent("NHcalc", () => NHcalc);
